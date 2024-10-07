0e17f76503cc1bf92713e6e728fed3cc6f345f37
DearNoodle <wan619395@gmail.com>
2024-10-07 13:28:03 +0800

    fix: cor settings

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Authorization'],
   }),
 );
 
