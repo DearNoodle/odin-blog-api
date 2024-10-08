ed7ff889d4ba80cd509a6f2950af60923f111994
DearNoodle <wan619395@gmail.com>
2024-10-08 20:52:28 +0800

    test

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ app.use(
       'http://localhost:5173',
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Authorization'],
   }),
 );
 
